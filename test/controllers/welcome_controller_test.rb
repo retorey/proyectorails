ba8de3b209280052f343a376b0cb61362859cb59
andres <andres.c.arismendi@gmail.com>
2017-03-27 22:00:56 -0300

    primer commit

---
+++ test/controllers/welcome_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class WelcomeControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get asd" do
+    get :asd
+    assert_response :success
+  end
+
+end
